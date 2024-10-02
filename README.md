5de800e662daeb886a32b9eb58e5367b02c621f9
Colin Cai <colincai09@gmail.com>
2024-10-01 21:38:35 -0700

    refactor secret api config storage

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 - Node.js
 - pip
 
-## Installation
+## Frontend Initialization
 
 1. Clone this repository
 
@@ -31,4 +31,33 @@ REACT_APP_GEMINI_API_KEY=your_gemini_api_key
 ```bash
 cd bot-overflow/frontend &&
 npm start
+```
+
+## Backend Initialization
+
+1. Clone this repository
+
+```bash
+git clone git@github.com:bot-overflow/bot-overflow.git
+```
+
+2. Create a new conda environment (Optional)
+```base 
+conda create --name env_name_here  python=3.9  
+conda activate env_name_here
+```
+
+3. Install the required packages
+
+```bash
+pip install -r bot-overflow/backend/requirements.txt 
+```
+
+4. Add api key in bot-overflow/backend/app_api_key_config.yaml file
+
+5. Start the server
+
+```bash
+cd bot-overflow/backend &&
+flask run
 ```
--- backend/config.py
+++ backend/config.py
@@ -3,20 +3,23 @@ import yaml
 import warnings
 
 class Config:
-    def __init__(self, config_file='app_config.yaml'):
+    def __init__(self, config_file='app_config.yaml', api_key_config_file='app_api_key_config.yaml'):
         # Load the YAML file
         with open(config_file, 'r') as file:
             config_data = yaml.safe_load(file)
+
+        with open(api_key_config_file, 'r') as file:
+            api_config_data = yaml.safe_load(file)
 
         # Database settings
         self.SQLALCHEMY_DATABASE_URI = config_data.get('database', {}).get('uri', os.getenv('DATABASE_URL', 'sqlite:///app.db'))
         self.SQLALCHEMY_TRACK_MODIFICATIONS = config_data.get('database', {}).get('track_modifications', False)
 
         # API Keys
-        self.OPENAI_API_KEY = config_data.get('api_keys', {}).get('openai_api_key', os.getenv('OPENAI_API_KEY'))
-        self.ANTHROPIC_API_KEY = config_data.get('api_keys', {}).get('anthropic_api_key', os.getenv('ANTHROPIC_API_KEY'))
-        self.HF_API_KEY = config_data.get('api_keys', {}).get('hf_api_key', os.getenv('HF_API_KEY'))
-        self.GEMINI_API_KEY = config_data.get('api_keys', {}).get('gemini_api_key', os.getenv('GEMINI_API_KEY'))
+        self.OPENAI_API_KEY = api_config_data.get('api_keys', {}).get('openai_api_key', os.getenv('OPENAI_API_KEY'))
+        self.ANTHROPIC_API_KEY = api_config_data.get('api_keys', {}).get('anthropic_api_key', os.getenv('ANTHROPIC_API_KEY'))
+        self.HF_API_KEY = api_config_data.get('api_keys', {}).get('hf_api_key', os.getenv('HF_API_KEY'))
+        self.GEMINI_API_KEY = api_config_data.get('api_keys', {}).get('gemini_api_key', os.getenv('GEMINI_API_KEY'))
 
         # Check if any API key is missing and issue a warning
         if not self.OPENAI_API_KEY:
