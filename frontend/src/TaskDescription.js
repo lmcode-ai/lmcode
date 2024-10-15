0c60caae80dac98138744ee5241f58e7b095f59a
Jie Qiu <jieqiu0630@gmail.com>
2024-10-14 23:11:11 -0400

    Extract copy function

--- frontend/src/TaskDescription.js
+++ frontend/src/TaskDescription.js
@@ -8,6 +8,7 @@ import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { copyToClipboard } from './utils/text';
 
 SyntaxHighlighter.registerLanguage('python', python);
 SyntaxHighlighter.registerLanguage('java', java);
@@ -106,34 +107,6 @@ const TaskDescription = ({ task }) => {
 
   const handleToggleExample = () => {
     setShowExample(!showExample);
-  };
-
-  const copyToClipboard = (text) => {
-    if (navigator.clipboard && window.isSecureContext) {
-      // If we are in a secure context, use the Clipboard API
-      try {
-        navigator.clipboard.writeText(text);
-        alert('Example input copied to clipboard!');
-      } catch (err) {
-        alert('Unable to copy to clipboard', err);
-      }
-    } else {
-      const textArea = document.createElement("textarea");
-      textArea.value = text;
-      textArea.style.top = "0";
-      textArea.style.left = "0";
-      textArea.style.position = "fixed";
-      document.body.appendChild(textArea);
-      textArea.focus();
-      textArea.select();
-      try {
-        document.execCommand('copy');
-        alert('Example input copied to clipboard!');
-      } catch (err) {
-        alert('Unable to copy to clipboard', err);
-      }
-      document.body.removeChild(textArea);
-    }
   };
 
   return (
