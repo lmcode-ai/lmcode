1df6ca1897f1d1c013de7821335f531ebf8bb208
Jie Qiu <jieqiu0630@gmail.com>
2024-10-27 18:38:42 -0400

    Revert

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// TODO: add strict mode back
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
